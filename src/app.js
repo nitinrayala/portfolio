f26306a586f8cb1bdf6472c02f891d7a0dac8d63
nitinrayala <nitinrayala@gmail.com>
2026-04-24 19:22:06 +0530

    Align navbar filename casing and update import

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Home from './pages/home';
 import Projects from './pages/projects';
 import './app.css';
